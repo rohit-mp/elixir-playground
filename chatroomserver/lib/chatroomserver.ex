93114014abcbcdee2d0a2d7b6bc139f7dca1a0e8
Rohit M P <rohitmp99@gmail.com>
2019-09-01 11:56:16 +0530

    Works as intended; need to check up on exiting telnet connection

--- chatroomserver/lib/chatroomserver.ex
+++ chatroomserver/lib/chatroomserver.ex
@@ -22,22 +22,38 @@ defmodule Chatroomserver do
     {:ok, socket} = :gen_tcp.listen(port, [:binary, packet: :line, active: false, reuseaddr: true])
     Logger.info("Accepting connection on port #{port}")
     {:ok, client_list} = Agent.start(fn -> [] end)
-    loop_acceptor(socket, client_list)
+    {:ok, name_map} = Agent.start(fn -> %{} end)
+    loop_acceptor(socket, client_list, name_map)
   end
 
-  defp loop_acceptor(socket, client_list) do
+  defp loop_acceptor(socket, client_list, name_map) do
     {:ok, client} = :gen_tcp.accept(socket)
-    Agent.update(client_list, fn list -> list = [client | list] end)
-    spawn(fn -> serve(client, client_list) end)
-    loop_acceptor(socket, client_list)
+    name = get_name(client)
+    Agent.update(client_list, fn list -> [client | list] end) 
+    Agent.update(name_map, fn map -> Map.put(map, client, name) end)
+    spawn(fn -> serve(client, client_list, name_map) end)
+    loop_acceptor(socket, client_list, name_map)
   end
 
-  defp serve(client, client_list) do
-    data = read_line(client)
-    list = Agent.get(client_list, fn list -> list end)
-    IO.inspect(list)
-    Enum.each(list, fn x -> if x != client, do: :gen_tcp.send(x, data) end)
-    serve(client, client_list)
+  defp get_name(client) do
+    :gen_tcp.send(client, "Enter your name: ")
+    {:ok, name} = :gen_tcp.recv(client, 0)
+    name = name |> String.replace("\r", "") |> String.replace("\n", "")
+    IO.inspect(name)
+    name
+  end
+
+  defp serve(client, client_list, name_map) do
+    read_data = read_line(client)
+    if read_data == ":exit\r\n" do
+      Agent.update(client_list, fn list -> Enum.filter(list, fn x -> x != client end) end)
+      Agent.update(name_map, fn map -> Map.delete(map, client) end)
+    else
+      send_data = Agent.get(name_map, fn map -> Map.get(map, client) end) <> ": " <> read_data
+      list = Agent.get(client_list, fn list -> list end)
+      Enum.each(list, fn x -> if x != client, do: :gen_tcp.send(x, send_data) end)
+      serve(client, client_list, name_map)
+    end
   end
 
   defp read_line(client) do
