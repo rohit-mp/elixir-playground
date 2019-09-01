e1112f1861d9774d60fdd2b585812b20b09c3b61
Rohit M P <rohitmp99@gmail.com>
2019-09-01 11:18:19 +0530

    Chat room works barely

--- chatroomserver/lib/chatroomserver.ex
+++ chatroomserver/lib/chatroomserver.ex
@@ -1,6 +1,5 @@
 defmodule Chatroomserver do
   require Logger
-  import IEx.Helpers
   use Agent
   @moduledoc """
   Documentation for Chatroomserver.
@@ -22,62 +21,23 @@ defmodule Chatroomserver do
   def accept(port \\ 4041) do
     {:ok, socket} = :gen_tcp.listen(port, [:binary, packet: :line, active: false, reuseaddr: true])
     Logger.info("Accepting connection on port #{port}")
-    names = spawn(fn -> name_map(%{}) end)
-    clients = spawn(fn -> client_list([]) end)
-    Process.register(names, :names)
-    Process.register(clients, :client)
-    loop_acceptor(socket, :names)
+    {:ok, client_list} = Agent.start(fn -> [] end)
+    loop_acceptor(socket, client_list)
   end
 
-  defp name_map(map) do 
-    receive do
-      {:get, key, caller} -> 
-        send(caller, Map.get(map, key))
-        name_map(map)
-      {:get_all, caller} ->
-        send(caller, map)
-        name_map(map)
-      {:put, key, value} ->
-        name_map(Map.put(map, key, value))
-    end
-  end 
-
-  defp client_list(list) do
-    receive do
-      {:add, value} -> client_list([value | list])
-      {:remove, value} -> client_list(Enum.reject(list, fn x -> x == value end))
-    end
-  end
-
-  defp loop_acceptor(socket, :names, :clients) do
+  defp loop_acceptor(socket, client_list) do
     {:ok, client} = :gen_tcp.accept(socket)
-    # send(:clients, )
-    spawn(fn -> get_name(client, :names) end)
-    loop_acceptor(socket, :names, :clients)
+    Agent.update(client_list, fn list -> list = [client | list] end)
+    spawn(fn -> serve(client, client_list) end)
+    loop_acceptor(socket, client_list)
   end
 
-  defp get_name(client, :names, :clients) do 
-    :gen_tcp.send(client, "Enter your name: ")
-    {:ok, name} = :gen_tcp.recv(client, 0)
-    send(:names, {:put, client, name})
-    send(:clients, {:add, client})
-    serve(client, :names, name, :clients)
-  end 
-
-  defp serve(client, :names, name, :clients) do
+  defp serve(client, client_list) do
     data = read_line(client)
-    case data do
-      :exit -> send(:clients, {:remove, client})
-    send(:names, {:get_all, self()})
-    receive do
-    Enum.each(flush(), fn a -> 
-      cond do
-        a == :ok -> :ok
-        {k, v} != {client, _} -> :gen_tcp.send(k, v <> ": " <> data)
-        k == client -> :ok
-      end 
-    end)
-    serve(client, :names, name, :clients)
+    list = Agent.get(client_list, fn list -> list end)
+    IO.inspect(list)
+    Enum.each(list, fn x -> :gen_tcp.send(x, data) end)
+    serve(client, client_list)
   end
 
   defp read_line(client) do
