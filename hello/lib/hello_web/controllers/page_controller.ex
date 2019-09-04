c5f617321396e42bbb25645edafe29fde470f1c5
Rohit M P <rohitmp99@gmail.com>
2019-09-04 09:39:29 +0530

    Added another webpage

--- hello/lib/hello_web/controllers/page_controller.ex
+++ hello/lib/hello_web/controllers/page_controller.ex
@@ -5,3 +5,16 @@ defmodule HelloWeb.PageController do
     render(conn, "index.html")
   end
 end
+
+defmodule HelloWeb.HelloController do
+  use HelloWeb, :controller
+
+  def index(conn, _params) do
+    render(conn, "index.html")
+  end
+
+  def show(conn, %{"messenger" => messenger}) do
+    render(conn, "show.html", messenger: messenger)
+  end
+end
+
